08743face984acb8a0367f0eb8caf135217770ef
Ohmaha40 <keller.chris@outlook.de>
2021-03-03 22:06:40 +0100

    bad verbessert

--- Beleuchtung/Deckenlicht_Bad.js
+++ Beleuchtung/Deckenlicht_Bad.js
@@ -1,24 +1,12 @@
 "use strict"
-on ({id: bewegungsmelder, change: "ne"}, function () {
-    let bewegungsmelder = "0_userdata.0.Beleuchtung.Badezimmer.Bewegungsmelder";
-    let rolladen_zu = "0_userdata.0.Rolladensteuerung.Rolladen_Bad.Rolladen_Bad_ist_Zu";
-    let lichtlevel_bad = "alias.0.Badezimmer.Lichtlevel_Bad.ACTUAL";
-    let nische_rechts = "hue.0.Badezimmer_Nische.on";
-    let nische_links = "hue.0.Badezimmer_Nische_vorne.on";
-    if (getState(bewegungsmelder).val && (getState(lichtlevel_bad).val > 40 || getState(rolladen_zu).val == true)){
-        setState(nische_rechts, true);
-        setState(nische_links, true);
+on ({id: new RegExp("0_userdata.0.Beleuchtung.Badezimmer.Bewegungsmelder" + "|" + "0_userdata.0.Beleuchtung.Badezimmer.Licht_Nische"), change: "ne"}, async function (obj) {
+    if (obj.state.val && (getState("alias.0.Badezimmer.Lichtlevel_Bad.ACTUAL").val > 40 || getState("0_userdata.0.Rolladensteuerung.Rolladen_Bad.Rolladen_Bad_ist_Zu").val == true)){
+        setState("hue.0.Badezimmer_Nische.on", true);
+        setState("hue.0.Badezimmer_Nische_vorne.on", true);
+        setState("0_userdata.0.Beleuchtung.Badezimmer.Licht_Nische", true);
     } else {
-        setState(nische_rechts, false);
-        setState(nische_links, false);
+        setState("hue.0.Badezimmer_Nische.on", false);
+        setState("hue.0.Badezimmer_Nische_vorne.on", false);
+        setState("0_userdata.0.Beleuchtung.Badezimmer.Licht_Nische", false);
     }
 });
-
-// on ({id: '0_userdata.0.Beleuchtung.Badezimmer.Bewegungsmelder', change: "ne"}, function () {
-//     if (getState("0_userdata.0.Beleuchtung.Badezimmer.Bewegungsmelder").val && (getState("alias.0.Badezimmer.Lichtlevel_Bad.ACTUAL").val > 40 || getState("0_userdata.0.Rolladensteuerung.Rolladen_Bad.Rolladen_Bad_ist_Zu").val == true)) {
-//         setState("0_userdata.0.Beleuchtung.Badezimmer.Licht_Nische",true);
-//     }else if(getState("0_userdata.0.Beleuchtung.Badezimmer.Bewegungsmelder").val == false) {
-//         setState("alias.0.Badezimmer.Deckenlicht_Bad.SET",false);
-//         setState("0_userdata.0.Beleuchtung.Badezimmer.Licht_Nische",false);
-//     }
-// });
