ddb5986060d22a4402187e80b0935ddef960606b
Ohmaha40 <keller.chris@outlook.de>
2021-03-08 20:11:04 +0100

    bad soweit

--- Beleuchtung/Deckenlicht_Bad.js
+++ Beleuchtung/Deckenlicht_Bad.js
@@ -1,8 +1,8 @@
 "use strict"
-// createState("beleuchtung.bad.dusche.Startzeit", function () {});
-// createState("beleuchtung.bad.dusche.Endzeit", function () {});
-// createState("beleuchtung.bad.dusche.Dauer", function () {});
-// createState("beleuchtung.bad.dusche.gesamt_Dauer", function () {});
+createState("beleuchtung.bad.spiegel.Startzeit", function () {});
+createState("beleuchtung.bad.spiegel.Endzeit", function () {});
+createState("beleuchtung.bad.spiegel.Dauer", function () {});
+createState("beleuchtung.bad.spiegel.gesamt_Dauer", function () {});
 const badezimmer = {
     //schalter und bedingungen 
     bewegungsmelder: {
@@ -47,51 +47,72 @@ const badezimmer = {
             dauer_t: "javascript.0.beleuchtung.bad.dusche.Dauer",
             dauer_t_gesamt: "javascript.0.beleuchtung.bad.dusche.gesamt_Dauer",
             leistung: 15
-        },
+                },
+        decke: {
+            licht:  "sonoff.0.Schalter_Badlicht", //.on
+            level: "",
+            ct: "",
+            on: ".POWER",
+            vis: "0_userdata.0.Beleuchtung.Badezimmer.Licht_Decke",
+            start_t: "javascript.0.beleuchtung.bad.decke.Startzeit",
+            ende_t:  "javascript.0.beleuchtung.bad.decke.Endzeit",
+            dauer_t: "javascript.0.beleuchtung.bad.decke.Dauer",
+            dauer_t_gesamt: "javascript.0.beleuchtung.bad.decke.gesamt_Dauer",
+            leistung: 15
+              },
+        spiegelschrank: {
+            licht:  "shelly.0.SHSW-1#A4CF12F47D41#1.Relay0", //.on
+            level: "",
+            ct: "",
+            on: ".Switch",
+            vis: "0_userdata.0.Beleuchtung.Badezimmer.Licht_Spiegel",
+            start_t: "javascript.0.beleuchtung.bad.spiegel.Startzeit",
+            ende_t:  "javascript.0.beleuchtung.bad.spiegel.Endzeit",
+            dauer_t: "javascript.0.beleuchtung.bad.spiegel.Dauer",
+            dauer_t_gesamt: "javascript.0.beleuchtung.bad.spiegel.gesamt_Dauer",
+            leistung: 15
+                },
     // leuchte Dusche
     // Methoden
      start(geraet_s, obj) {         // String für Gerät übergeben und anschalte true ausschalten false
         let geraet;
-        let on;
+        let an;
         let time;
         switch (geraet_s){
             case "nische_1":
                 geraet = this.nische_1;
-                on = false;
+                an = false;
                 time = true;
-                // if (obj && getState(geraet.licht + a.on).val == false) {
-                //     this.einschalten(new Date(), a, false, true);
-                //     } else {
-                //         this.auschalten(new Date(), a, true);
-                //     }
                 break;
             case "nische_2":
                 geraet = this.nische_2;
-                on = false;
+                an = false;
                 time = false;
-                // if (obj && getState(a.licht + a.on).val == false) {
-                //     this.einschalten(new Date(), a, false, true);
-                //     } else {
-                //         this.auschalten(new Date(), a, false);
-                //     }
                 break;
             case "dusche":
                 geraet = this.dusche;
-                on = true;
+                an = true;
                 time = true;
                 break;
             case "decke":
+                geraet = this.decke;
+                an = true;
+                time = true;
                 break;
-        }
+            case "spiegelschrank":
+                geraet = this.spiegelschrank;
+                an = true;
+                time = true;
+                }
         if (obj && getState(geraet.licht + geraet.on).val == false) {
-            this.einschalten(new Date(), geraet, on, time);
+            this.einschalten(new Date(), geraet, an, time);
             } else {
                 this.auschalten(new Date(), geraet, time);
             }
-    },
+        },
     einschalten(startzeit, geraet, on, time_setzen) {       // Startzeit übergeben, gerät übergeben, on == false gibt an das farbe und level übergeben wird, ob die zeit gesetzt werden soll(bei mehreen lampen)
         if (on) {
-            setState(geraet.licht, true);
+            setState(geraet.licht + geraet.on, true);
             setState(geraet.vis, true);
             if (time_setzen) {
                 setState(geraet.start_t, startzeit);
@@ -104,7 +125,7 @@ const badezimmer = {
                 setState(geraet.start_t, startzeit);
                 }
             }
-    },
+        },
     auschalten(endzeit, geraet, zeit_setzen) {              // Startzeit übergeben, gerät übergeben, ob die zeit gesetzt werden soll(bei mehreen lampen)
         setState(geraet.licht + geraet.on, false);
         setState(geraet.vis, false);
@@ -113,13 +134,12 @@ const badezimmer = {
             setTimeout(function() {
                     badezimmer.zeit_berechnen(geraet);
                     },2000);
-                }
-            
-    },
+                }  
+        },
     zeit_berechnen(geraet) {
         setState(geraet.dauer_t, dauer(getState(geraet.start_t).val, getState(geraet.ende_t).val));
         setState(geraet.dauer_t_gesamt, gesamtLaufzeitTage(getState(geraet.dauer_t_gesamt).val, dauer(getState(geraet.start_t).val, getState(geraet.ende_t).val)));
-    },
+        },
     bewegung(obj) {
         if (obj) {
             setState(this.bewegungsmelder.vis, true);
@@ -127,22 +147,28 @@ const badezimmer = {
             var tbwm = setTimeout(() => {
                 setState(this.bewegungsmelder.vis, false);     
             }, 5 * 60000);
+            }
         }
-    }
 };
 on({id: badezimmer.bewegungsmelder.motion, change: "ne"}, async function (obj) {
     badezimmer.bewegung(obj.state.val);
-});
+    });
 on({id: badezimmer.bewegungsmelder.vis, change: "ne"}, async function (obj) {
     if (getState(badezimmer.bewegungsmelder.level).val < 40 || !getState(badezimmer.rolladen_zu)) {
     badezimmer.start("nische_1", obj.state.val);
     badezimmer.start("nische_2", obj.state.val);
-    }
-});
+        }
+    });
 on({id: badezimmer.nische_1.vis, change: "ne"}, async function (obj) {
     badezimmer.start("nische_1", obj.state.val);
     badezimmer.start("nische_2", obj.state.val);
-});
+    });
 on({id: badezimmer.dusche.vis, change: "ne"}, async function (obj) {
     badezimmer.start("dusche", obj.state.val);
-});
+    });
+on({id: badezimmer.decke.vis, change: "ne"}, async function (obj) {
+    badezimmer.start("decke", obj.state.val);
+    });
+on({id: badezimmer.spiegelschrank.vis, change: "ne"}, async function (obj) {
+    badezimmer.start("spiegelschrank", obj.state.val);
+    });
