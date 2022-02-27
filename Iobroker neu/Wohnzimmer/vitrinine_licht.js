9ea52b715dc2f95a5b94d0d92d98073adba01ae9
Ohmaha40 <keller.chris@outlook.de>
2022-02-27 10:55:17 +0100

    vitrine

---
+++ Iobroker neu/Wohnzimmer/vitrinine_licht.js
@@ -0,0 +1,23 @@
+"use strict"
+const licht = {
+    vitrine:{
+        schalter: "0_userdata.0.Vis.Wohnzimmer.vitrine_licht",
+        leuchte: "tuya.0.4530056170039f4b2dfc.1",
+        rückmeldung: "",
+        zeiten:{
+            einschaltzeit: "05:00",
+            ausschaltzeit: "22:00",
+            timer: "",
+        }
+    }
+};
+on({id: licht.vitrine.schalter, change: "ne"}, async function (obj){
+    let val = obj.state.val;
+    setState(licht.vitrine.leuchte, val);
+});
+schedule('{"time":{"exactTime":true,"start":"18:00"},"period":{"days":1}}', async function () {
+    setState(licht.vitrine.schalter, true);
+});
+schedule('{"time":{"exactTime":true,"start":"08:00"},"period":{"days":1}}', async function () {
+    setState(licht.vitrine.schalter, false);
+});
